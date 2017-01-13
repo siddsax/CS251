4dd7c7be066072e27548f6de1d9e2194681a510f
siddsax <ss.siddharthasaxena@gmail.com>
2017-01-13 15:30:44 +0530

    error detetion added in Ass_1

--- Assignment_1/Compare.sh
+++ Assignment_1/Compare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+if [[ $# != 2 ]];then 
+	echo "You have entered less or more than 2 numbers"
+	exit 1
+fi
 if [[ "$1" > "$2" ]];
 then 
   echo "$1 > $2"
@@ -7,5 +10,5 @@ elif [[ "$1" = "$2" ]]
 then 
   echo "$2 = $1"
 else [[ "$1" < "$2" ]]
-  echo "$2 > $1" 
+  echo "$1 < $2" 
 fi
--- Assignment_1/search.sh
+++ Assignment_1/search.sh
@@ -1,4 +1,13 @@
 	#!/bin/bash
+if [[ $# != 3 ]];then 
+	echo "You have not entered the file name or the two substrings to search"
+	exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "First argument is not a valid file name!"
+    exit 1
+fi
 
 string_a="$(grep -n $2 $1 | cut -d : -f 1)"
 string_b="$(grep -n $3 $1 | cut -d : -f 1)"
