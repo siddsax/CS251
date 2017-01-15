fe3bea053b87b210021e23f1338e4c22d3efb64c
siddsax <ss.siddharthasaxena@gmail.com>
2017-01-15 20:36:40 +0530

    correcting for floats in compare

--- Assignment_1/Compare.sh
+++ Assignment_1/Compare.sh
@@ -3,12 +3,14 @@ if [[ $# != 2 ]];then
 	echo "You have entered less or more than 2 numbers"
 	exit 1
 fi
-if [[ "$1" > "$2" ]];
+var_1="$(echo $1'>'$2 | bc -l)"
+var_2="$(echo $1'<'$2 | bc -l)"
+if (( "$var_1" == 1 ))
 then 
   echo "$1 > $2"
-elif [[ "$1" = "$2" ]]
+elif (( "$var_2" == 1 ))
 then 
-  echo "$2 = $1"
-else [[ "$1" < "$2" ]]
-  echo "$1 < $2" 
+  echo "$1 < $2"
+else 	
+  echo "$2 = $1" 
 fi
